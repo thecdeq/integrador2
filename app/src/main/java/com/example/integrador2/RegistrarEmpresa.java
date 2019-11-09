4454ad6a523a4ddc1acf1df76ca8c8535374b09e
Diego <lobito962014@gmail.com>
2019-11-09 17:54:21 -0500

    regsitra los tres usuarios

--- app/src/main/java/com/example/integrador2/RegistrarEmpresa.java
+++ app/src/main/java/com/example/integrador2/RegistrarEmpresa.java
@@ -1,15 +1,28 @@
 package com.example.integrador2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarEmpresa extends AppCompatActivity {
 
@@ -34,24 +47,91 @@ public class RegistrarEmpresa extends AppCompatActivity {
 
         //inicializamos el objeto firebaseAuth
         firebaseAuth = FirebaseAuth.getInstance();
-        Log.e("error",firebaseAuth.toString());
+        Log.e("error", firebaseAuth.toString());
         //Referenciamos los views
         Text_NombreEmpr = (EditText) findViewById(R.id.Txt_NombreEmpr);
         Text_Rubro = (EditText) findViewById(R.id.Txt_Rubro);
         Text_RUC = (EditText) findViewById(R.id.Txt_RUC);
         TextEmailEmpre = (EditText) findViewById(R.id.TxtEmailEmpre);
         TextPasswordRegEmpre = (EditText) findViewById(R.id.TxtPasswordRegEmpre);
-
-
         btnRegistrarEmpresa = (Button) findViewById(R.id.botonRegistrarEmpresa);
-        btnRegistrarEmpresa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //codigo de cloud  Firestore
-            }
-        });
 
         progressDialog = new ProgressDialog(this);
 
+        btnRegistrarEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrarUsuario();
+                //codigo de cloud  Firestore
+            }
+        });
     }
+
+        private void registrarUsuario() {
+
+            //Obtenemos el email y la contraseña desde las cajas de texto
+            final String email = TextEmailEmpre.getText().toString().trim();
+            String password = TextPasswordRegEmpre.getText().toString().trim();
+
+            //Verificamos que las cajas de texto no esten vacías
+            if (TextUtils.isEmpty(password) | password.length() < 5) {
+                Toast.makeText(this, "contraseña invalida", Toast.LENGTH_LONG).show();
+                return;
+            }
+            //verificamos que las cajas de texto no esten vacias
+            if (TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "Se debe ingresar un email valido", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            progressDialog.setMessage("Realizando registro en linea...");
+            progressDialog.show();
+
+            //creating a new user
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    registrar();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                    Toast.makeText(RegistrarEmpresa.this, "Error el usuario no pudo ser creado: "+e.toString(), Toast.LENGTH_LONG).show();
+                }
+            });
+
+        }
+
+    void registrar() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("tipo", "empresa"); // persona / empresa / profesional
+        usuarios.put("nombre", Text_NombreEmpr.getText().toString());
+        usuarios.put("rubro", Text_Rubro.getText().toString());
+        usuarios.put("ruc", Text_RUC.getText().toString());
+
+
+
+
+        db.collection("usuarios").document(TextEmailEmpre.getText().toString()).set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+            @Override
+            public void onSuccess(Void aVoid) {
+                progressDialog.dismiss();
+                startActivity(new Intent(RegistrarEmpresa.this, HomeEmpresa.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegistrarEmpresa.this, "Error los datos del usuario no fueron guardados", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+        }
 }
--- app/src/main/java/com/example/integrador2/RegistrarProfesional.java
+++ app/src/main/java/com/example/integrador2/RegistrarProfesional.java
@@ -1,20 +1,32 @@
 package com.example.integrador2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrarProfesional extends AppCompatActivity {
     //defining view objects
@@ -25,7 +37,6 @@ public class RegistrarProfesional extends AppCompatActivity {
     private EditText TextPasswordRegProfe;
     private Button btnRegistrarProfes;
     private ProgressDialog progressDialog;
-
 
 
     //Declaramos un objeto firebaseAuth
@@ -39,28 +50,91 @@ public class RegistrarProfesional extends AppCompatActivity {
 
         //inicializamos el objeto firebaseAuth
         firebaseAuth = FirebaseAuth.getInstance();
-        Log.e("error",firebaseAuth.toString());
+        Log.e("error", firebaseAuth.toString());
         //Referenciamos los views
         Text_NombreProfe = (EditText) findViewById(R.id.Txt_NombreProfe);
         Text_ApellidoProfe = (EditText) findViewById(R.id.Txt_ApellidoProfe);
-        TextProfesion= (EditText) findViewById(R.id.Txt_Profesion);
+        TextProfesion = (EditText) findViewById(R.id.Txt_Profesion);
         TextEmailProfe = (EditText) findViewById(R.id.TxtEmailProfe);
         TextPasswordRegProfe = (EditText) findViewById(R.id.TxtPasswordRegProfe);
-
-
         btnRegistrarProfes = (Button) findViewById(R.id.botonRegistrarProfe);
+
+        progressDialog = new ProgressDialog(this);
+
         btnRegistrarProfes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                registrarUsuario();
                 //codigo de cloud  Firestore
 
             }
         });
 
+    }
 
-        progressDialog = new ProgressDialog(this);
+    private void registrarUsuario() {
 
+        //Obtenemos el email y la contraseña desde las cajas de texto
+        final String email = TextEmailProfe.getText().toString().trim();
+        String password = TextPasswordRegProfe.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if (TextUtils.isEmpty(password) | password.length() < 5) {
+            Toast.makeText(this, "contraseña invalida", Toast.LENGTH_LONG).show();
+            return;
+        }
+        //verificamos que las cajas de texto no esten vacias
+        if (TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Se debe ingresar un email valido", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        progressDialog.setMessage("Realizando registro en linea...");
+        progressDialog.show();
+
+        //creating a new user
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                registrar();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegistrarProfesional.this, "Error el usuario no pudo ser creado: " + e.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
 
+
+    void registrar() {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("tipo", "profesional"); // persona / empresa / profesional
+        usuarios.put("nombre", Text_NombreProfe.getText().toString());
+        usuarios.put("apellido", Text_ApellidoProfe.getText().toString());
+        usuarios.put("profesion", TextProfesion.getText().toString());
+
+
+        db.collection("usuarios").document(TextEmailProfe.getText().toString()).set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+            @Override
+            public void onSuccess(Void aVoid) {
+                progressDialog.dismiss();
+                startActivity(new Intent(RegistrarProfesional.this, HomeProfesional.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(RegistrarProfesional.this, "Error los datos del usuario no fueron guardados", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
 }
